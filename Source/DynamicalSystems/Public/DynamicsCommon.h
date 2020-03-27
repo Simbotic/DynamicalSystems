857b60500e1ba7e2a32ec751dedd1c37fa0ec925
Alex Rozgo <alex.rozgo@gmail.com>
2020-03-27 16:23:24 -0600

    initial commit

---
+++ Source/DynamicalSystems/Public/DynamicsCommon.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EngineUtils.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Kismet/KismetRenderingLibrary.h"
+#include "Components/PrimitiveComponent.h"
+#include "DynamicsCommon.generated.h"
+
+UCLASS()
+class DYNAMICALSYSTEMS_API UDynamicsCommon : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+  public:
+
+    UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Math|Float")
+    static float MeanOfFloatArray(const TArray<float> &Samples);
+
+    UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Math|Float")
+    static float VarianceOfFloatArray(const TArray<float> &Samples);
+
+    UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Math|Float")
+    static float MedianOfFloatArray(const TArray<float> &Samples);
+
+    UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Math|Float")
+    static float StandardDeviationOfFloatArray(const TArray<float> &Samples);
+
+    UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Math|Rotator")
+    static FQuat RotatorToQuat(FRotator Rotator);
+
+    UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Math|Vector")
+    static FVector CubicBezier(float Time, FVector P0, FVector P1, FVector P2, FVector P3);
+};
